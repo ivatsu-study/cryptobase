7a4cc1bad6373b66d7f87dbcacc4a9324ad79fc8
IvaTsu <ivan.tsud@gmail.com>
2021-12-07 21:00:53 +0200

    Create basic layout and routing, add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Space, Typography } from 'antd';
+
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Cryptobase</h1>
+    <div className="app">
+      <Navbar />
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <footer className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Cryptobase <br/ > All rights are reserved!</Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import HomePage from "./HomePage";
+import Exchanges from "./Exchanges";
+import Cryptocurrencies from "./Cryptocurrencies";
+import CryptoDetails from "./CryptoDetails";
+import News from "./News";
+
+export { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News }
