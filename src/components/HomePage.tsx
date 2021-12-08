da65736cdcf05bf30a608fcfbd750bcedebc0a9d
IvaTsu <ivan.tsud@gmail.com>
2021-12-08 21:27:17 +0200

    Create Layout for Fetching data at HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,21 @@
+import { Fragment } from 'react';
+import { Typography, Col, Statistic } from 'antd';
+
+const { Title } = Typography;
+
 const HomePage = () => {
   return (
-    <div>Homepage</div>
+    <Fragment>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Col span={12}>
+        {/* TODO: fetch and pass `value`s */}
+        <Statistic title="Total Cryptocurencies" value="5" />
+        <Statistic title="Total Exchanges" value="5" />
+        <Statistic title="Total Market Cap" value="5" />
+        <Statistic title="Total 24h Volume" value="5" />
+        <Statistic title="Total Markets" value="5" />
+      </Col>
+    </Fragment>
   )
 }
 
